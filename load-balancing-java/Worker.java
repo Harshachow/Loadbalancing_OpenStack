6065ed688a4a1db122e886f7c95c1782e6b5579e
Harshachow <42604140+Harshachow@users.noreply.github.com>
2021-01-20 11:23:59 +0530

    Add files via upload

---
+++ load-balancing-java/Worker.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.*;
+import java.util.*;
+import java.io.*;
+
+public class Worker {
+    public static void main(String[] args) {
+        try {
+            // Load MySQL JDBC Driver.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "Green@123");
+
+            // Open socket for this worker.
+            ServerSocket workerSocket = new ServerSocket(Integer.valueOf(args[0]));
+            while(true){
+                // Accept connection from Load Balancer.
+                Socket loadBalancerSocket = workerSocket.accept();
+                
+                // Start a new thread to service this request.
+                Thread workerTask = new Thread(new WorkerTask(loadBalancerSocket, conn));
+                workerTask.start();
+            }
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
